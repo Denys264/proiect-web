83a1fb670c25b2f629ff3fbd63426518d1f3b581
Denys264 <148255556+Denys264@users.noreply.github.com>
2024-01-17 12:03:03 +0200

    Add files via upload

--- Flask tutorial/website/__init__.py
+++ Flask tutorial/website/__init__.py
@@ -6,11 +6,18 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+def create_database(app):
+    if not path.exists("website/" + DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print("Created database!")
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "helloworld"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    
+    # Initialize the db within the app
     db.init_app(app)
 
     from .views import views
@@ -32,8 +39,3 @@ def create_app():
         return User.query.get(int(id))
 
     return app
-
-def create_database(app):
-    if not path.exists("website/" + DB_NAME):
-        db.create_all(app=app)
-        print("Created database!")
